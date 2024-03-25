5282a67772d24c43011972e0692c50d88f8715b4
Seshu916 <pavanseshu001@gmail.com>
2024-03-25 19:30:58 +0530

    this code commit i fixed all the bugs

---
+++ Objectex3.java
@@ -0,0 +1,11 @@
+class Student{
+    void method(){
+        System.out.println("welcome to method");
+    }
+}
+class Objectex3{
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.method();
+    }
+}
---
+++ Switchex1.java
@@ -0,0 +1,16 @@
+class Switchex1{
+    public static void main(String[] args){
+        int num=10;
+        switch(num){
+            case 7:
+            System.out.println("welcome to excelr"+ num);
+            break;
+            case 10:
+            System.out.println("welcome to excelr"+num);
+            break;
+            default:
+            System.out.println("no matches");
+            break;
+        }
+    }
+}
